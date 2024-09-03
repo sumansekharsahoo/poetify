82d8642e53a703bb939f233742dcecee86fac1a1
root <root@ip-172-31-47-33.ap-south-1.compute.internal>
2024-09-03 05:56:00 +0000

    Fixed small auth bug

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -11,10 +11,12 @@ export const isAuthenticated = () => {
 };
 
 export const getUserId=()=>{
-  return localStorage.getItem('poetifytoken')[0];
+  	let id = localStorage.getItem('poetifytoken').split(',')[0];
+	console.log(typeof(id));
+	return id;
 }
 
 export const getUserName=()=>{
-  let len=localStorage.getItem('poetifytoken').length;
-  return localStorage.getItem('poetifytoken').substring(2,len);
+  let name = localStorage.getItem('poetifytoken').split(',')[1];
+  return name;
 }
